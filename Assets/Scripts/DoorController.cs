d263874475842b5b6f9776a8acad7b1d113d2e54
alejobalboa <alejo.balboa@yahoo.com.ar>
2024-11-14 04:13:33 -0300

    DoorController Refactor, DoorSounds added

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -3,10 +3,8 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     [SerializeField] private Animator doorAnimator;  
-    [SerializeField] private string requiredKeyName1;  
-    [SerializeField] private string requiredKeyName2;  
-    [SerializeField] private string animation1 = "Door_Hinge_Open";  
-    [SerializeField] private string animation2 = "Door_Hinge_Open2"; 
+    [SerializeField] private string requiredKeyName;   
+    [SerializeField] private string doorOpeningAnimation;  
     [SerializeField] private float detectionRange = 2f;
     
     [Header("Sounds")] 
@@ -20,21 +18,14 @@ public class DoorController : MonoBehaviour
 
     private void Update()
     {
-        // Verificar si el jugador está dentro del rango de detección
         if (Vector3.Distance(transform.position, PlayerController.PlayerSingleton.transform.position) < detectionRange)
         {
-            // Verificar si se presiona la tecla 'E' y si el jugador tiene la primera o la segunda llave
             if (Input.GetKeyDown(KeyCode.E))
             {
-                if (PlayerController.PlayerSingleton.inventory.HasKey(requiredKeyName1))
+                if (PlayerController.PlayerSingleton.inventory.HasKey(requiredKeyName))
                 {
-                    OpenDoor(animation1); 
-                    PlayerController.PlayerSingleton.inventory.UseKey(requiredKeyName1);
-                }
-                else if (PlayerController.PlayerSingleton.inventory.HasKey(requiredKeyName2))
-                {
-                    OpenDoor(animation2);
-                    PlayerController.PlayerSingleton.inventory.UseKey(requiredKeyName2);
+                    OpenDoor(doorOpeningAnimation); 
+                    PlayerController.PlayerSingleton.inventory.UseKey(requiredKeyName);
                 }
             }
         }
@@ -42,7 +33,7 @@ public class DoorController : MonoBehaviour
 
     private void OpenDoor(string animationName)
     {
-        _SoundControl.PlaySound(DoorSound);
         doorAnimator.SetTrigger(animationName);
+        _SoundControl.PlaySound(DoorSound);
     }
 }
